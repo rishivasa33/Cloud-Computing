6120801a3b48bb26ddca1148358f95aac932195b
Rishi Vasa <rs310602@dal.ca>
2023-02-05 20:15:16 -0400

    Initial code to validate request, file and calculate checksum with hardcoded local paths

---
+++ A1/ChecksumCalculatorMicroservice2/ChecksumCalculatorMS2.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const fs = require('fs');
+const crypto = require('crypto');
+
+const PORT = 8081;
+
+const app = express();
+app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+//Reference to calculate MD5 hash of a file in Node: https://www.debugpointer.com/nodejs/create-md5-hash-of-file-in-nodejs
+
+app.post('/', (request, response) => {
+    console.log("Request Recieved: " + JSON.stringify(request.body));
+
+    const filePath = '../' + request.body.file;
+
+    const fileBuffer = fs.readFileSync(filePath);
+    const md5Hash = crypto.createHash("md5").update(fileBuffer).digest("hex");
+    console.log("MD5 Hash: " + md5Hash);
+    return response.json({ "file": request.body.file, "checksum": md5Hash });
+});
+
+// starting the server
+app.listen(PORT, () => {
+    console.log(`Running on port: ${PORT}`);
+});
---
+++ A1/ValidatorMicroservice1/APIHandlerMS1.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const axios = require('axios');
+const fs = require('fs');
+
+const PORT = 5000;
+
+const app = express();
+app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("Hello, Welcome to Rishi's CSCI 5409 - Assignment 1! - Validator Microservice 1");
+});
+
+//Reference for sending API Requests in Node: https://www.npmjs.com/package/axios 
+app.post('/checksum', (request, response) => {
+
+    console.log("Request Recieved: " + JSON.stringify(request.body));
+    if (request.body.file) {
+
+        const path = '../' + request.body.file;
+
+        if (fs.existsSync(path)) {
+            console.log('File found');
+        } else {
+            console.log('File not found');
+            return response.json({ "file": null, "error": "File not found." });
+        }
+
+        let config = {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+        axios.post('http://localhost:8081/', request.body, config)
+            .then(function (checksumResponse) {
+                return response.send(checksumResponse.data);
+            })
+
+    } else {
+        response.json({ "file": null, "error": "Invalid JSON input." })
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Running on port: ${PORT}`);
+});
