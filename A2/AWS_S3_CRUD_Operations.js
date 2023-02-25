41d2680702d609b8749733f5ebfbbea74f4c8c23
Rishi Vasa <rs310602@dal.ca>
2023-02-25 04:30:51 -0400

    Removed unwanted dependancies

--- A2/AWS_S3_CRUD_Operations.js
+++ A2/AWS_S3_CRUD_Operations.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const helmet = require('helmet');
 const AWS = require('aws-sdk');
 
 const PORT = 80;
